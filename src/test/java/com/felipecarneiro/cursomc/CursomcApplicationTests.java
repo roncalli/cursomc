3393bfc175fcdded746dfa40ee1298a52cf6b130
Felipe Carneiro <feliperoncalli@gmail.com>
2022-02-17 13:28:57 -0300

    Projeto criado

---
+++ src/test/java/com/felipecarneiro/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipecarneiro.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
