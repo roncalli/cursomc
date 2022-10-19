5a397b8d524f067b1f71f7cb9c376e366a409600
Felipe Carneiro <feliperoncalli@gmail.com>
2022-10-19 15:30:18 -0300

    Validacao sintatica com Beans Validation

--- src/main/java/com/felipecarneiro/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/felipecarneiro/cursomc/dto/CategoriaDTO.java
@@ -2,6 +2,11 @@ package com.felipecarneiro.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.felipecarneiro.cursomc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable {
@@ -12,6 +17,9 @@ public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 	
 	public CategoriaDTO() {
